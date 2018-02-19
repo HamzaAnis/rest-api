a247116ae4a8d9cba74d9d43f43afc89aff78c4d
Hamza Anis <hamzaanis9514@gmail.com>
2018-02-19 17:25:28 +0500

    Response getting ✔✔

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 	"math/big"
 	"os"
 	"time"
+
+	"github.com/parnurzeal/gorequest"
 )
 
 type restAPI struct {
 	config []config
+	complete chan struct{}
+	//A chan to wait for all the routines
 }
 type config struct {
 	ApiEndPoint string `json:"APiEndPoint"`
@@ -21,6 +25,8 @@ type config struct {
 	Username    string `json:"Username"`
 	HostName    string `json:"HostName"`
 	Port        string `json:"Port"`
+	Start       string `json:"Start"`
+	End 		string `json:"End"`
 }
 
 func readConfig() []config {
@@ -35,21 +41,35 @@ func readConfig() []config {
 	return d
 }
 func main() {
+	request := gorequest.New()
+	fmt.Println("Calling")
+	_, body, err := request.Post("http://qa-sc-a-api.entertainmentbook.com.au/api/1.0/epic/fundraisers/").Set("content-type", "application/json").
+		Set("Authorization", "KIsPsSSPP0fCW61aQF0iKeTtoZaNDPQmwSEPXOu94Bae3oRPfbwn8H5s9OVt99XIjSNBavaqSrU57pGetN0KolJ1YwcNfkqhzKK6").
+		End()
+	fmt.Println("Calling2")
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	fmt.Printf("Response Body: \n%s\n", body)
 	api := restAPI{
 		config: readConfig(),
+		complete:  make(chan struct{}),
 	}
 
 	start := time.Now()
-
 	r := new(big.Int)
 	fmt.Println(r.Binomial(1000, 10))
-
+	
 	//A main function
 	api.startRead(900374, 900375, "files")
 
 	elapsed := time.Since(start)
 	log.Printf("Binomial took %s", elapsed)
+
+	for i := 1; i <= 2; i++ {
+		<-api.complete
+	}
 }
 
 //read files iterate over the files and read the file
@@ -57,20 +77,15 @@ func main() {
 func (api restAPI) startRead(lower int, upper int, location string) {
 	//A chan where the data will be sent
 	dataChan := make(chan string)
-	//A chan to wait for all the routines
-	c := make(chan struct{})
-
 	go api.handleData(dataChan)
 	for i := 1; i <= 2; i++ {
 		//Format to read the file
 		file := fmt.Sprintf("%s%s%d%s", location, "/Fundraiser_900375.", i, ".txt")
-		go api.readFile(file, dataChan, c)
+		go api.readFile(file, dataChan)
 	}
-	for i := 1; i <= 2; i++ {
-		<-c
-	}
+
 }
-func (api restAPI) readFile(file string, dataChan chan string, wait chan struct{}) {
+func (api restAPI) readFile(file string, dataChan chan string) {
 	raw, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Println(err.Error)
@@ -79,28 +94,28 @@ func (api restAPI) readFile(file string, dataChan chan string, wait chan struct{
 	// fmt.Println("\t\tYESY", api.config[0].ApiEndPoint)
 	// spew.Dump(d)
 	dataChan <- string(raw)
-	wait <- struct{}{} // signal that the routine has completed
+	
 
 }
 func (api restAPI) handleData(dataChan <-chan string) {
 	for {
 		select {
 		case paylord := <-dataChan:
-			go makeRequest(paylord, api.config[0].ApiEndPoint, api.config[0].ApiKey, api.config[0].ContentType)
+			go makeRequest(paylord, api.config[0].ApiEndPoint, api.config[0].ApiKey, api.config[0].ContentType,api.complete)
 		}
 	}
 }
 
-func makeRequest(paylord string, endpoint string, apiKey string, contentType string) {
+func makeRequest(paylord string, endpoint string, apiKey string, contentType string,complete chan struct{}) {
 	request := gorequest.New()
 	fmt.Println("Calling")
 	_, body, err := request.Post(endpoint).Set("content-type", contentType).
 		Set("Authorization", apiKey).
 		End()
+	fmt.Println("Calling2")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Calling2")
-
 	fmt.Printf("Response Body: \n%s\n", body)
+	complete <- struct{}{} // signal that the routine has completed
 }
